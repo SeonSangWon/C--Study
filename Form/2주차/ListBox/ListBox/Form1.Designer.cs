085d88958e7a1433bba1ca33e7405aa34ac8944a
선상원 <45645824+SeonSangWon@users.noreply.github.com>
2019-09-25 12:21:10 +0900

    Add files via upload

---
+++ Form/2주차/ListBox/ListBox/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbView = new System.Windows.Forms.ListBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbView
+            // 
+            this.lbView.FormattingEnabled = true;
+            this.lbView.ItemHeight = 16;
+            this.lbView.Location = new System.Drawing.Point(54, 29);
+            this.lbView.Name = "lbView";
+            this.lbView.Size = new System.Drawing.Size(181, 244);
+            this.lbView.TabIndex = 0;
+            this.lbView.SelectedIndexChanged += new System.EventHandler(this.lbView_SelectedIndexChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(51, 293);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(59, 16);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "결과 : ";
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(270, 29);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(181, 23);
+            this.txtList.TabIndex = 2;
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtList_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(270, 78);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(181, 45);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 360);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbView);
+            this.Font = new System.Drawing.Font("문체부 제목 바탕체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "리스트 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbView;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Form/2주차/ListView/ListView/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //문자열 저장
+        string strName, strAge, strWork;
+
+        private void lvView_Click(object sender, EventArgs e)
+        {
+            if (this.lvView.SelectedItems.Count == 0)
+                MessageBox.Show("아이템을 선택하세요.!", "알림", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                MessageBox.Show("이름 : " + this.lvView.SelectedItems[0].SubItems[0].Text +
+                    "\n나이 : " + this.lvView.SelectedItems[0].SubItems[1].Text +
+                    "\n직업 : " + this.lvView.SelectedItems[0].SubItems[2].Text, "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (TextCheck() == true)
+            {
+                strName = this.txtName.Text;
+                strAge = this.txtAge.Text;
+                strWork = this.txtWork.Text;
+
+                this.txtName.Text = "";
+                this.txtAge.Text = "";
+                this.txtWork.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("아이템을 입력하세요.!", "알림",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+                
+
+            ListViewItem lvi = new ListViewItem(new string[] { strName, strAge, strWork });
+            this.lvView.Items.Add(lvi);
+        }
+
+        private bool TextCheck()
+        {
+            if (this.txtName.Text != "" && this.txtAge.Text != "" && txtWork.Text != "")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void txtWork_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (this.txtName.Text != "" && this.txtAge.Text != "" && this.txtWork.Text != "")
+                {
+                    strName = this.txtName.Text;
+                    strAge = this.txtAge.Text;
+                    strWork = this.txtWork.Text;
+
+                    this.txtName.Text = "";
+                    this.txtAge.Text = "";
+                    this.txtWork.Text = "";
+
+                    ListViewItem lvi = new ListViewItem(new string[] { strName, strAge, strWork });
+                    this.lvView.Items.Add(lvi);
+                }
+                else
+                {
+                    MessageBox.Show("아이템을 입력하세요.!", "알림",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
